88beb006495164574061d547e66a9d65fe031d48
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-15 02:54:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,3 +11,26 @@ dictionary = {
   at: "@",
   and: "&"
 }
+
+def word_substituter (tweet)
+  dictionary = {
+    hello: "hi",
+    to: "2",
+    too: "2",
+    two: "2",
+    four: "4",
+    for: "4",
+    be: "b",
+    you: "you",
+    at: "@",
+    and: "&"
+  }
+  tweet_array = []
+  tweet_array = tweet.split(" ")
+  tweet_array.each do |word|
+    if word == dictionary.keys
+      word = dictionary.values
+    end
+    tweet_array.join(" ")
+  end
+end
